3f9837502183069d81a5559df43f3c025f815e22
mukuru1 <kabengaedwin@gmail.com>
2024-11-02 14:04:09 +0200

    edited content

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -12,4 +12,17 @@ function App() {
     </div>
   );
 }
+
+
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+  );
+}
+
 export default App;
