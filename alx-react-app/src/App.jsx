b3bd3cce39fef6affb26bc983cc004cf2d175251
mukuru1 <kabengaedwin@gmail.com>
2024-11-02 14:06:29 +0200

    edited content

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
