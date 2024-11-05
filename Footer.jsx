42fb136c72c87695922179819b77006fe76d62d9
mukuru1 <kabengaedwin@gmail.com>
2024-11-05 20:02:32 +0200

    implemented content for files

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+// src/components/Footer.jsx
+function Footer() {
+    return (
+      <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+        <p>&copy; 2023 Our Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
