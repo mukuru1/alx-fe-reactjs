2eafd9fe32d46a5c23b6705b73e07998e93da544
mukuru1 <kabengaedwin@gmail.com>
2024-11-05 12:30:08 +0200

    style content

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
-       <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}></header>
+    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
       <h1>My Favorite Cities</h1>
     </header>
   );
 }
+
 export default Header;
