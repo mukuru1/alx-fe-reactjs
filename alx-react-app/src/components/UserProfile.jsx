aa366909dd4b8d39c2af7287d143154e6a5bb259
mukuru1 <kabengaedwin@gmail.com>
2024-11-05 12:47:23 +0200

    added style content

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const UserProfile = (props) => {
+function UserProfile(props) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue', fontSize: '1.5em', margin: '5px 0' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+      <p style={{ marginTop: '5px' }}>Bio: {props.bio}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
