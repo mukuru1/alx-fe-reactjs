ac34d63b28933709a22b52211378720c3066473f
mukuru1 <kabengaedwin@gmail.com>
2024-11-05 13:31:59 +0200

    added style content to the copy files

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f9f9f9', color: '#333', textAlign: 'justify' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
