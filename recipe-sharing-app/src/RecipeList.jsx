b45f97bdc8ccacdb83eb9e55e4f07d9094cf9558
mukuru1 <kabengaedwin@gmail.com>
2024-11-12 14:03:40 +0200

    content for recipelist file

---
+++ recipe-sharing-app/src/RecipeList.jsx
@@ -0,0 +1,20 @@
+// src/components/RecipeList.jsx
+import useRecipeStore from '../recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
