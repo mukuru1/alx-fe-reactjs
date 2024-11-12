15e00d098dbbcd1e519efde001cbd32a1fe77552
mukuru1 <kabengaedwin@gmail.com>
2024-11-12 15:31:09 +0200

    updated app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,8 @@
-i// src/App.jsx
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-
 const App = () => {
   return (
     <div>
@@ -17,5 +15,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
