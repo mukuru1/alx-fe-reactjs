ed2ae32a0879c162d6da3b9e34992ef5f8c8d48a
mukuru1 <kabengaedwin@gmail.com>
2024-11-19 19:59:15 +0200

    imported and used registrationform component

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import RegistrationForm from './components/RegistrationForm';
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
+    <div className="App">
+      <h1>User Registration</h1>
+      <RegistrationForm />
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,5 +34,4 @@ function App() {
     </>
   )
 }
-
 export default App
