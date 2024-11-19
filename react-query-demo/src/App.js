bfd3d88dee726e42ce2a4ea2f07e9e72c16a0053
mukuru1 <kabengaedwin@gmail.com>
2024-11-19 21:20:00 +0200

    updated app.js with postscomponent

--- react-query-demo/src/App.js
+++ react-query-demo/src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './components/PostsComponent';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import axios from 'axios';
 const queryClient = new QueryClient();
+const fetchPosts = async () => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return response.data;
+};
+const PostsComponent = () => {
+  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading posts.</div>;
+  return (
+    <div>
+      <button onClick={refetch}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -13,3 +35,4 @@ function App() {
   );
 }
 export default App;
+
