1bb3f43c5077b57cd87c1997e4a2414aca7dd79b
mukuru1 <kabengaedwin@gmail.com>
2024-11-20 13:36:50 +0200

    updated file profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,22 @@
+import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 const Profile = () => {
   return (
     <div>
       <h2>Profile</h2>
       <nav>
-        <Link to="details">Profile Details</Link> | 
-        <Link to="settings">Profile Settings</Link>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
       </nav>
       <Outlet />
     </div>
   );
 };
 export default Profile;
+
