11c5c8cbe262b05e56d0f81c6058a791ad8f3f68
mukuru1 <kabengaedwin@gmail.com>
2024-11-20 12:06:51 +0200

    Simulated authentication in file app.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,12 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Profile from './Profile';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
+import BlogPost from './BlogPost';
+import ProtectedRoute from './ProtectedRoute';
+
+const isAuthenticated = false;
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </Router>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,5 +61,4 @@ function App() {
     </>
   )
 }
-
 export default App
