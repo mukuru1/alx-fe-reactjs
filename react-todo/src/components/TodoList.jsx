6b23fb00cfcabfb54d9d1c59052ac380831485f9
mukuru1 <kabengaedwin@gmail.com>
2024-11-21 18:07:06 +0200

    updated todolist

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,45 +1,32 @@
 import React, { useState } from 'react';
 
-const TodoList = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Build Todo App', completed: true },
-  ]);
-  const [newTodo, setNewTodo] = useState('');
+function TodoList() {
+    const [todos, setTodos] = useState([
+        { id: 1, text: 'Learn React', completed: false },
+        { id: 2, text: 'Build a Todo List', completed: false },
+    ]);
 
-  const addTodo = () => {
-    if (newTodo.trim()) {
-      const newTodoItem = { id: Date.now(), text: newTodo, completed: false };
-      setTodos([...todos, newTodoItem]);
-      setNewTodo('');
-    }
-  };
+    const addTodo = (text) => {
+        const newTodo = { id: Date.now(), text, completed: false };
+        setTodos([...todos, newTodo]);
+    };
 
-  const toggleTodo = (id) => {
-    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
-  };
-  const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-  return (
-    <div>
-      <h2>Todo List</h2>
-      <input
-        type="text"
-        placeholder="Add a new todo"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>Add Todo</button>
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-            <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    const toggleComplete = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+    return (
+        <div>
+            {/* ... (Add Todo Form, Todo List, etc.) */}
+        </div>
+    );
+}
 export default TodoList;
