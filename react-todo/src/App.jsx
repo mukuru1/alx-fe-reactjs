133ee668d4a954ca8b33562f337bde5478a6d651
mukuru1 <kabengaedwin@gmail.com>
2024-11-21 17:34:25 +0200

    Updated App.jsx file

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Todo App</h1>
+      <TodoList />
+    </div>
+  );
+}
+export default App;
