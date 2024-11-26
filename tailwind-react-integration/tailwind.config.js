9f694016080adea942c956815d446915b7afe22e
mukuru1 <kabengaedwin@gmail.com>
2024-11-26 15:45:26 +0200

    Tailwind CSS and Its Dependencies

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 
