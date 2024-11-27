fc018ec9a3d7f876feccf68749c962077e52ccbb
mukuru1 <kabengaedwin@gmail.com>
2024-11-27 11:16:54 +0200

    Styling a Provided React Component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from "./components/UserProfile";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="App">
+      <UserProfile />
+    </div>
     <div className="h-screen flex items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-bold text-blue-600">
         Tailwind CSS is Working!
