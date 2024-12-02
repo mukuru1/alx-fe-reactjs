c4c9faee93732dd64881cbf72cbaed68c51068aa
mukuru1 <kabengaedwin@gmail.com>
2024-12-02 09:55:26 +0200

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], 
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
