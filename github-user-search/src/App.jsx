3aadf2a2cd01993cd7e4353fdc339a567c443b03
mukuru1 <kabengaedwin@gmail.com>
2024-12-09 14:40:45 +0200

    Working with APIs

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Search from './components/Search';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div>
+            <h1>GitHub User Search</h1>
+            <Search />
+        </div>
     <div>
             <h1>GitHub User Search Application</h1>
         </div>
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/users';
+
+const fetchUserData = async (username) => {
+    if (!username) throw new Error('Username is required');
+    const response = await axios.get(`${BASE_URL}/${username}`);
+    return response.data;
+};
+
+export default fetchUserData;
